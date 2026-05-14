e2d69eae6ff75506549f2c3a34d1290ca5715e42
pedrolucas30 <pedro.rodrigues358@aluno.ce.gov.br>
2026-05-14 16:58:49 -0300

    legal

---
+++ conexao.php
@@ -0,0 +1,3 @@
+<?php
+$connect = new PDO("mysql:host=localhost;dbname=trabalho_pw","root" , "");
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+include 'conexao.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcionários</title>
+</head>
+<body>
+    <h1>Funcionários</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>nome</th>
+                <th>data</th>
+                <th>horas</th>
+                <th>botões</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>delete</td>
+                <td>update</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+</table>
+
+</body>
+</html>
