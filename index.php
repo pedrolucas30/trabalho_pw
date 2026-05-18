f3966d046da16ee70f733a2b6eeab281924bfdd1
unknown <alicelimadocarmo@outlook.com>
2026-05-18 11:10:48 -0300

    commit

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php 
-include 'conexao.php';
+    include 'conexao.php';
+
+    if(isset($_GET['delete_id'])) {
+        $id_delete = $_GET['delete_id'];
+
+        $stmt = $connect->prepare("DELETE FROM funcionários WHERE id = :id ");
+        $stmt->bindValue(':id', $id_delete);
+
+        if ($stmt->execute()) {
+            header("Location: index.php");
+            exit();
+        }
+    }
+
+
+    $query = connect->prepare("SELECT * FROM funcionários");
+    $query->execute();
+    $lista = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,6 +28,7 @@ include 'conexao.php';
     <title>Funcionários</title>
 </head>
 <body>
+   <a href="create.php"><button> Adicionar categoria </button></a>
     <h1>Funcionários</h1>
     <table>
         <thead>
@@ -21,15 +41,23 @@ include 'conexao.php';
             </tr>
         </thead>
         <tbody>
+            <?php foreach ($lista as $item): ?>
             <tr>
-                <td>delete</td>
-                <td>update</td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-</table>
+                <td><?php echo $item['id']; ?></td>
+                <td><?php echo $item['nome']; ?></td>
+                <td><?php echo $item['data']; ?></td>
+                <td><?php echo $item['horas']; ?></td>
+                <td>
+                    <a href="update.php?id=<?php echo $item['id']; ?>"><button>Editar</button></a>
+                    <a href="index.php?delete_id=<?php echo $item['id'];  ?> "onclick="return confirm('Deseja mesmo excluir')">
+                    <button>Excluir</button></a>
 
+
+                </td>
+            
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
---
+++ storyU.php
@@ -0,0 +1,14 @@
+<?php 
+
+include 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $id = $_POST['id'];
+    $atualizar = $_POST['atualizar'];
+
+    $stmt
+}
+
+
+?>
