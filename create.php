8819d901e7fb32ef2ceb414ff9c82c4c63e0199b
unknown <alicelimadocarmo@outlook.com>
2026-05-19 08:03:21 -0300

    foi

---
+++ create.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar</title>
+
+</head>
+<body>
+     <form action="storyC.php" method="post">
+        <label for="name">Adicionar novo funcionário</label>
+        <input type="text" name ="funcionário" id ="funcionário">
+        <button type="submit">Salvar</button>
+        <a href="index.php"><button type="button">Cancelar</button></a>
+     </form>      
+</body>
+</html>
