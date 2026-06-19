c71c2a84288da5aea7083fdf9404e5593303fc53
unknown <alicelimadocarmo@outlook.com>
2026-06-19 10:12:50 -0300

    mudança no aqv conexao

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php
-$connect = new PDO("mysql:host=localhost;dbname=trabalho_pw;charset=utf8", "root", "");
+$connect = new PDO("mysql:host=db;dbname=trabalho_pw;charset=utf8", "root", "root");
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
